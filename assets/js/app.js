eebb343e4c76ee782a4ab0c37e89d28f035dbd9b
Gerardo <gerardo.meirone@gmail.com>
2021-10-02 22:43:46 -0300

    link a diferentes secciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
     console.log("ready!");
+
+    $('[data-toggle="tooltip"]').tooltip();
+    console.log("holaaaaa boton")
+
     $(window).scroll(function () {
         if ($("#navbar").offset().top > 56) {
             $("#navbar").addClass("navbar-color");
@@ -7,4 +11,14 @@ $(document).ready(function () {
             $("#navbar").removeClass("navbar-color");
         }
     });
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+    
+
+    
+
   });
